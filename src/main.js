9507cc013648dc2da34e840ebff0816f34b4ba72
yuki <12012640@mail.sustech.edu.cn>
2023-03-08 18:32:39 +0800

    yuki - change

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-plus'
 import 'element-plus/dist/index.css'
-import router from './router'
+import router from '/router'
 
 createApp(App).use(ElementUI).use(router).mount('#app')
+
