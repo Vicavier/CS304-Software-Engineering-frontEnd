d198e7c890bf82d13fd5430076054eb989cc104f
yuki <12012640@mail.sustech.edu.cn>
2023-03-08 14:53:01 +0800

    yuki - add login and register page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+// import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/theme-chalk/index.css'
+
+
 
 createApp(App).use(ElementUI).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+// const { defineConfig } = require('@vue/cli-service')
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
   transpileDependencies: true
 })
+
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
